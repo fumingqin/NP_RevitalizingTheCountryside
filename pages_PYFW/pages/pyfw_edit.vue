fc77b50306260822cca20528e73822f3ae5cdd79
傅洺钦 <32589407@qq.com>
2020-10-27 10:54:59 +0800

    1

--- pages_PYFW/pages/pyfw_edit.vue
+++ pages_PYFW/pages/pyfw_edit.vue
@@ -6,22 +6,17 @@
 
 					<!-- 乡村名 -->
 					<u-form-item :label-style="customStyle" :label-position="labelPosition" label="乡村名" :border-bottom="false" prop="name">
-						<view class="viewClass" style="padding-right: 20rpx;">
-							<u-input :custom-style="tradeNameStyle" :border="false" placeholder="请选择乡村名" v-model="model.name" type="text"></u-input>
+						<view class="viewClass">
+							<input style="height: 96upx; padding-left: 24upx;" placeholder="点击选择乡村名" v-model="villageName" disabled="true"
+							 @click="VillageShow = true" />
 						</view>
 					</u-form-item>
 
-					<!-- 商品来源地 -->
-					<!-- <u-form-item :label-style="customStyle" :label-position="labelPosition" label="." :border-bottom="false" prop="region">
-					<view class="viewClass" style="padding-right: 20rpx;">
-						<u-input :custom-style="tradeNameStyle" :border="false" type="select" :select-open="pickerShow" v-model="model.region" placeholder="请选择商品来源地" @click="pickerShow = true"></u-input>
-					</view>
-				</u-form-item> -->
-
 					<!-- 技术类型 -->
-					<u-form-item :label-style="customStyle" :label-position="labelPosition" label="问题类型" :border-bottom="false" prop="cost">
-						<view class="viewClass" style="padding-right: 20rpx;">
-							<u-input :custom-style="tradeNameStyle" :border="false" placeholder="请选择问题类型" v-model="model.cost" type="text"></u-input>
+					<u-form-item :label-style="customStyle" :label-position="labelPosition" label="问题类型" :border-bottom="false">
+						<view class="viewClass">
+							<u-input :custom-style="tradeNameStyle" :border="false" v-model="techValue" type="select" @click="techShow = true"></u-input>
+							<u-action-sheet :list="techList" v-model="techShow" @click="actionSheet"></u-action-sheet>
 						</view>
 					</u-form-item>
 
@@ -37,7 +32,7 @@
 
 					<!-- 简要备注 -->
 					<u-form-item :label-style="customStyle" :label-position="labelPosition" label="问题内容" :border-bottom="false" prop="intro">
-						<view class="viewClass" style="padding-right: 20rpx;">
+						<view class="viewClass">
 							<u-input :custom-style="textareaStyle" type="textarea" :height="200" :auto-height="autoHeight" :border="border"
 							 placeholder="请描述遇到的问题" :maxlength="50000" v-model="model.intro" />
 						</view>
@@ -45,10 +40,45 @@
 				</u-form>
 			</view>
 		</view>
+		<!-- 防触底空模块 -->
+		<view style="width: 100%; height: 112upx;"></view>
+
 		<!-- 提交申请 -->
 		<view class="operButton">
 			<text class="buttonView2" @click="submit">提交申请</text>
 		</view>
+
+		<u-popup v-model="VillageShow" mode="center">
+			<view>
+				<!-- 顶部搜索框 -->
+				<view class="topSerchView">
+					<view class="SearchBar" elevation='5px'>
+						<input class="addressInput" @input="onInput" placeholder="请输入关键字搜索" />
+					</view>
+				</view>
+
+
+				<!-- 搜索列表 -->
+				<view class="stationList" :style="{ 'height':scrollHeight }" v-if="SearchStatus">
+					<block v-for="(item,index) in VillageSearchList" :key="index">
+						<view class="listItem" @click="Listclick(item)">
+							<rich-text :nodes="item.village_name"></rich-text>
+						</view>
+					</block>
+				</view>
+
+				<!-- 原列表 -->
+				<view class="stationList" :style="{ 'height':scrollHeight }" v-if="VillageStatus">
+					<block v-for="(item,index) in VillageList" :key="index">
+						<view class="listItem" @click="Listclick(item)">
+							<rich-text :nodes="item.village_name"></rich-text>
+						</view>
+					</block>
+				</view>
+				
+			</view>
+		</u-popup>
+
 		<u-picker mode="region" v-model="pickerShow" @confirm="regionConfirm"></u-picker>
 	</view>
 </template>
@@ -57,18 +87,41 @@
 	export default {
 		data() {
 			return {
+				villageName: '点击选择乡村名', //乡村名
+				VillageShow: false, //乡村弹框
+				VillageSearchList: [], //关键字查询的乡村
+				VillageList: [], //乡村列表
+				VillageData: '', //乡村名数据缓存
+				VillageStatus: true, //原列表显示状态
+				SearchStatus: false, //搜索框显示状态
+
+
+				techShow: false, //技术类型选项栏开启状态
+				techValue: '请选择类型', //技术类型的默认值
+
 				model: {
 					name: '', //商品名称value
 					region: '', //选择来源地value
-					cost: '', //价格
+					type: '', //类型
 					photo: '', //图片
 					intro: '', //商品简介
 				},
+				techList: [{
+					text: '设备故障'
+				}, {
+					text: '设备维护'
+				}, {
+					text: '乡村种植'
+				}, {
+					text: '乡村帮扶'
+				}], //技术类型
+
 				//----------------uview样式--------------------------
 				customStyle: {
 					fontWeight: 'bold',
 					fontSize: '17px',
 					marginTop: '32rpx',
+					padding: '0rpx',
 				},
 				buttonStyle: {
 					marginTop: '20px',
@@ -107,17 +160,6 @@
 						message: '请选择商品来源地',
 						trigger: 'change',
 					}],
-					cost: [{
-							required: true,
-							message: '请输入价格',
-							trigger: ['change', 'blur'],
-						},
-						{
-							type: 'number',
-							message: '价格只能为数字',
-							trigger: ['change', 'blur'],
-						}
-					],
 					intro: [{
 							required: true,
 							message: '请填写简介'
@@ -148,7 +190,11 @@
 		onReady() {
 			this.$refs.uForm.setRules(this.rules);
 		},
-
+		
+		onLoad(){
+			this.lostData();
+		},
+		
 		methods: {
 			submit() {
 				//-----------------提交表单数据-----------------------
@@ -161,11 +207,68 @@
 				});
 			},
 
+			//-----------------请求乡村列表-----------------------
+			lostData: function() {
+				uni.request({
+					url: this.$pyfw.KyInterface.getVillageList.Url,
+					method: this.$pyfw.KyInterface.getVillageList.method,
+					success: (res) => {
+						uni.hideLoading();
+						console.log('乡村列表', res);
+						this.VillageList = res.data.data;
+					},
+					fail(res) {
+						uni.hideLoading();
+					}
+				})
+			},
+
 			//--------------------- 选择地区回调 --------------------------
 			regionConfirm(e) {
 				this.model.region = e.province.label + '-' + e.city.label + '-' + e.area.label;
 				console.log(this.model)
 			},
+
+			//选择技术类型回调
+			actionSheet: function(index) {
+				// console.log(index)
+				this.techValue = this.techList[index].text;
+			},
+
+			onInput: function(e) {
+				console.log('监听输入', e)
+				//以下示例截取淘宝的关键字，请替换成你的接口
+				if(e.detail.value != ''){
+					this.VillageStatus = false;
+					this.SearchStatus = true;
+				}else{
+					this.VillageStatus = true;
+					this.SearchStatus = false;
+				}
+				
+				uni.showLoading();
+				uni.request({
+					url: this.$pyfw.KyInterface.getVillageListByName.Url,
+					method: this.$pyfw.KyInterface.getVillageListByName.method,
+					data: {
+						keyName: e.detail.value
+					},
+					success: (res) => {
+						uni.hideLoading();
+						console.log('模糊搜索', res);
+						this.VillageSearchList = res.data.data;
+					},
+					fail(res) {
+						uni.hideLoading();
+					}
+				});
+			},
+
+			Listclick: function(e) {
+				this.villageName = e.village_name;
+				this.VillageData = e;
+			},
+
 		}
 	}
 </script>
@@ -218,6 +321,125 @@
 			color: #FFFFFF;
 			font-size: 32upx;
 			line-height: 3;
+		}
+	}
+
+	.topSerchView {
+		background-color: #DBDBDB;
+		padding-top: 20rpx;
+		padding-bottom: 20rpx;
+		width: 750upx;
+
+		.SearchBar {
+			background-color: #FFFFFF;
+			margin-right: 20rpx;
+			margin-left: 20rpx;
+			box-shadow: 0px 4px 9px 1px rgba(204, 204, 204, 1);
+			flex-direction: row;
+			border-radius: 9px;
+			padding: 20rpx;
+			align-items: center;
+			justify-content: space-between;
+		}
+
+		//地址搜索输入
+		.addressInput {
+			color: #999999;
+			font-size: 30rpx;
+			font-weight: 300;
+		}
+	}
+
+	//站点列表
+	.stationList {
+		background-color: #FFFFFF;
+		line-height: 80rpx;
+		box-sizing: border-box;
+		font-size: 28rpx;
+		height: 800rpx;
+
+		.listItem {
+			margin-left: 20rpx;
+			border-bottom: 1rpx solid #eeeeee;
+		}
+	}
+
+	.list_box {
+		display: flex;
+		flex-direction: row;
+		flex-wrap: nowrap;
+		justify-content: flex-start;
+		align-items: flex-start;
+		align-content: flex-start;
+		font-size: 28rpx;
+
+		.left {
+			width: 200rpx;
+			background-color: #f6f6f6;
+			line-height: 80rpx;
+			box-sizing: border-box;
+			font-size: 32rpx;
+			text-align: center;
+
+			.item {
+				padding-left: 20rpx;
+				position: relative;
+				padding: 8upx 0;
+
+				&:not(:first-child) {
+					margin-top: 1px;
+
+					&::after {
+						content: '';
+						display: block;
+						height: 0;
+						border-top: #d6d6d6 solid 1px;
+						width: 620upx;
+						position: absolute;
+						top: -1px;
+						right: 0;
+						transform: scaleY(0.5);
+						/* 1px像素 */
+					}
+				}
+
+				&.active,
+				&:active {
+					color: #42b983;
+					background-color: #fff;
+					padding: 8upx 0;
+				}
+			}
+		}
+
+		.main {
+			background-color: #fff;
+			padding-left: 20rpx;
+			width: 0;
+			flex-grow: 1;
+			box-sizing: border-box;
+
+			.swiper {
+				height: 500px;
+			}
+
+
+			.item {
+				padding-bottom: 10rpx;
+			}
+		}
+
+		.goods {
+			display: flex;
+			flex-direction: row;
+			flex-wrap: nowrap;
+			justify-content: flex-start;
+			align-items: center;
+			align-content: center;
+			margin-bottom: 10rpx;
+			border-bottom: 1rpx solid #eeeeee;
+			line-height: 100upx;
+			padding-left: 20upx;
 		}
 	}
 </style>
