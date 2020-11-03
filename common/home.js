ac399b256ab57b5b880d02c0f42272d4c8d1e3ae
傅洺钦 <32589407@qq.com>
2020-11-03 09:54:06 +0800

    1

--- common/home.js
+++ common/home.js
@@ -3,32 +3,42 @@ import $oSit from '@/common/overallSituation.js'; //引入公共js文件
 
 //接口域名
 // #ifdef H5
-	const Url=$oSit.Interface.address.Url;
+	const Url=$oSit.Interface.address.xczx_Url;
 // #endif
 // #ifndef H5
-	const Url=$oSit.Interface.address.Url;
+	const Url=$oSit.Interface.address.xczx_Url;
 // #endif
 
 //接口对象
 const KyInterface = {
 	//--------------------------------------轮播区--------------------------------------
-	GetRotationChart:{
-		Url: Url + '/api/BasicImage/GetRotationChart',
+	getImage:{
+		Url: Url + '/api/manage/getImage',
 		name:'轮播',
 		method:'POST',
 	}, 
-	//--------------------------------------资讯区--------------------------------------
-	GetHomeStyle:{
-		Url: Url + '/api/BasicImage/GetHomeStyle',
-		name:'资讯',
+	
+	//--------------------------------------跑马灯区--------------------------------------
+	getMarquee:{
+		Url: Url + '/api/manage/getMarquee',
+		name:'跑马灯',
 		method:'POST',
 	}, 
-	//--------------------------------------资讯通知--------------------------------------
-	GetNews:{
-		Url: Url + '/api/News/GetNews',
-		name:'资讯通知',
+	
+	//--------------------------------------水美经济--------------------------------------
+	getEconomy:{
+		Url: Url + '/api/economy/getEconomy',
+		name:'水美经济',
 		method:'POST',
 	}, 
+	
+	//--------------------------------------项目监督--------------------------------------
+	getProject:{
+		Url: Url + '/api/project/getProject',
+		name:'项目监督',
+		method:'POST',
+	}, 
+	
 	//--------------------------------------根据AID获取资讯--------------------------------------
 	GetNewsByAID:{
 		Url: Url + '/api/News/GetNewsByAID',
