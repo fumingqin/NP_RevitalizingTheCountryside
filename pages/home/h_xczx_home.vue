64888cbcbf946391438f961233d004db215417de
傅洺钦 <32589407@qq.com>
2020-12-07 16:34:53 +0800

    1

--- pages/home/h_xczx_home.vue
+++ pages/home/h_xczx_home.vue
@@ -71,11 +71,11 @@
 				</view>
 			</view>
 		</view>
-		<!-- @click="navigateToClick" -->
+		<!--  -->
 		<!-- 公示项目 -->
 		<view class="gs_view">
 			<view class="gs_title">公示项目</view>
-			<u-swiper class="gs_swiper" :list="advertisingMap" :effect3d="true" :title="true" bg-color="#ffffff"></u-swiper>
+			<u-swiper class="gs_swiper" :list="advertisingMap" :effect3d="true" :title="true" bg-color="#ffffff" @click="navigateToClick"></u-swiper>
 		</view>
 
 		<!-- 隐藏协议弹出 -->
